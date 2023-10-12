fc3253d2466f17011421e1972cadeb04539d80fe
Ben <meteoben@gmail.com>
2023-10-12 20:48:29 +0200

    refonte menu avec display flex en desktop

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+// var sidenav = document.getElementById("mySidenav");
+// var openBtn = document.getElementById("openBtn");
+// var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+// openBtn.onclick = openNav;
+// closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    sidenav.classList.add("active");
-}
+// /* Set the width of the side navigation to 250px */
+// function openNav() {
+//     sidenav.classList.add("active");
+// }
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    sidenav.classList.remove("active");
+// /* Set the width of the side navigation to 0 */
+// function closeNav() {
+//     sidenav.classList.remove("active");
+// }
+
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
