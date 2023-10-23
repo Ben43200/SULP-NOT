848d0a71751068d75aec56ecbfe85c8383f7785f
Ben <meteoben@gmail.com>
2023-10-23 16:45:45 +0200

    ajout du watch meiaqueries sur le script en commentaires résolution pb de passage desktop -> mobile liens soulignés

--- js/script.js
+++ js/script.js
@@ -49,3 +49,30 @@ window.onclick = function (event) {
         }
     }
 }
+
+
+
+
+// function myFunction2() {
+//     if (window.matchMedia("(min-width: 400px)").matches) {
+//         /* the view port is at least 400 pixels wide */
+//     } else {
+//         /* the view port is less than 400 pixels wide */
+//         document.getElementById("myDropdown").classList.toggle("show");
+
+//     }
+// }
+
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
